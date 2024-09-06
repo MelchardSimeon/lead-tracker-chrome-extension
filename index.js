88e1ef9b0ec3b753632a5dd99ca2f47acc30d7f1
MelchardSimeon <melchardsimeon71@gmail.com>
2024-09-06 14:22:41 +0800

    create function that can save input

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-let inputButton = document.getElementById("input-btn")
+let myLeads = [];
+let inputEl = document.getElementById("input-el")
+let inputValue = inputEl.value
+let inputButton = document.querySelector("#input-btn").addEventListener("click", saveInput)
+
+function saveInput() {
+    inputValue = inputEl.value
+    myLeads.push(inputValue)
+    console.log(myLeads)
+    inputEl.value = ""
+}
