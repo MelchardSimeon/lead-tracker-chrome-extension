db509003d4740d08dbf840be2eede44a05dfab16
MelchardSimeon <melchardsimeon71@gmail.com>
2024-09-06 23:22:02 +0800

    add anchor tags inside renderValue function

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ function renderValue() {
     ul.innerHTML = ""
     for (let i = 0; i < myLeads.length; i++) {
         let li = document.createElement("li")
-        li.textContent = myLeads[i]
+        let a = document.createElement("a")
+        a.href = myLeads[i];
+        a.target = "_blank"
+        a.textContent = myLeads[i];   
+        li.appendChild(a)
         ul.appendChild(li)
     }
 }       
